5e6fb047bc83b310ec97c66cd81350c036911db0
timmson <timmson666@mail.ru>
2022-11-19 12:49:50 +0300

    Initial commit

---
+++ merchant-view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  merchant-view
+//
+//  Created by Artem Krotov on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ merchant-viewUITests/merchant_viewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  merchant_viewUITestsLaunchTests.swift
+//  merchant-viewUITests
+//
+//  Created by Artem Krotov on 19.11.2022.
+//
+
+import XCTest
+
+final class merchant_viewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
