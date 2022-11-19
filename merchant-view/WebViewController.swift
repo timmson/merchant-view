57a42982421f6deafa1cd1b3195bd1c1be19eeb1
timmson <timmson666@mail.ru>
2022-11-19 14:41:32 +0300

    Add WebView

---
+++ merchant-view/WebViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewController.swift
+//  merchant-view
+//
+//  Created by Artem Krotov on 19.11.2022.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
